78b2c516abc676bd5129ffde2a8ac7a6dab40d1a
Subash Maharjan <hudeomah@gmail.com>
2018-03-05 19:37:33 -0500

    Adding carousel to homepage

--- pages/Works.vue
+++ pages/Works.vue
@@ -4,9 +4,8 @@
   <section class="section is-medium">
     <div class="container has-text-centered">
       <h1 class="title">My Works</h1>
-      <p>
-        Some of my projects can be found on <a href="https://github.com/submjn" target="_blank">GitHub</a>.
-      </p>
+      
     </div>
   </section>
 </template>
+
