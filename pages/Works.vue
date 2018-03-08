884065dfe184c5c2596ee74bab1e332c75ebffdd
Subash Maharjan <hudeomah@gmail.com>
2018-03-08 14:15:31 -0500

    adding view carousel

--- pages/Works.vue
+++ pages/Works.vue
@@ -4,8 +4,28 @@
   <section class="section is-medium">
     <div class="container has-text-centered">
       <h1 class="title">My Works</h1>
-      
+      <ul id="example-1">
+        <li v-for="item in items" :key="item.id">
+          {{ item.message }}
+        </li>
+      </ul>
     </div>
   </section>
 </template>
 
+<script>
+import Vue from 'vue';
+  var example1 = new Vue({
+  el: '#example-1',
+  data: {
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+});
+
+export default {
+  
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <!-- pages/index.vue -->
 
 <template>
-  <section class="section is-large">
+  <section class="section hero">
     <div class="container has-text-centered">
       <h1 class="title">Hi, I'm Subash Maharjan.</h1>
       <h2 class="subtitle">a Frontend Developer based in Charlottesville, Virginia.</h2>
